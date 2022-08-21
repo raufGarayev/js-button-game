212f99eed57a5d1b72ec16209075677e4eee3646
GARAYEV-PG <rafkagarayev@gmail.com>
2022-08-21 17:01:29 +0400

    commented for IT brains 118

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-const myBtn = document.querySelectorAll('button')
-const myDivs = document.querySelectorAll('div')
-const mike = document.getElementById('wazowski')
-const audio = new Audio("squeak.wav");
-let clicks = 0;
+const myBtn = document.querySelectorAll('button') //bütün butonları bir dəyişəndə saxlayırıq
+const myDivs = document.querySelectorAll('div') //eyni şəkildə bütün divləri dəyişəndə saxlayırıq
+const mike = document.getElementById('wazowski') //index.html-də yaratdığımız wazowski id-si olan classı seçirik
+const audio = new Audio("squeak.wav"); //fayllarda olan musiqini dəyişəndə saxlayırıq (qeyd: bu mövzü dərsdə keçilməyib sadəcə googledan baxdım)
+let clicks = 0; //bu dəyişəni yaradıb 0 dəyəri veririk, number olsun deyə. let-lə yaratmağımın səbəbi odurki dəyəri sonradan dəyişəcək
 
+
+//seçdiyimiz bütün butonların click eventini seçirik. (qeyd: forEach dərsdə keçilməyib hələ, googledan tapdım)
 myBtn.forEach(btn => {
     btn.addEventListener('click', startGame);
  });
 
+
+ //click eventinin funksiyasını başladırıq
 function startGame(e) {
-    clicks++
-    audio.play()
-    let rNum = Math.floor(Math.random() * 4);
+    clicks++ //bu bütün proqramlaşdırma dillərində olan bir üsuldur, dərsdə yaxında keçiləcək. click dəyişəni yuxarıda sıfır qeyd olunub, hər butona bastığımızda dəyəri +1 artır
 
-    const clickedButton = e.target
+    audio.play() //əlavə etdiyimiz səs faylını butona basdığımızda işə düşür
 
+    let rNum = Math.floor(Math.random() * 4); //buna googledan baxdım. 1dən 3ə qədər rastglə bir rəqəm verir bizə hər butona basıldığında
+
+    const clickedButton = e.target //dərsdə keçilən mövzü, buna həqiqətən ehtiyac varmiydi bilmirəm ama sonradan clicklədiyimiz divə class vermək üçün bu funksiyadan istifadə etdim
+
+    //clicklədiyimiz obyektdə btn classı varsa deyib yeni funksiya başladırıq
     if(clickedButton.classList.contains('btn')) {
 
+        //yuxarıda 1dən 3ə qədər rastgələ rəqəm istəmişdik, cavabı 1-sə əgər..ə
         if(rNum == 1) {
-            clickedButton.style.background = 'blue'
+            clickedButton.style.background = 'blue' //seçdiyimiz divə inline css əlavə edib arxa fon rəngini dəyişdiririk
         }
+        //yox əgər cavabı 2-sə..
         else if(rNum == 2) {
             clickedButton.style.background = 'red'
         }
@@ -28,13 +37,14 @@ function startGame(e) {
         }
     }
 
+    //dediyim kimi hər butona kliklədiyimizdə clicks dəyişəninin dəyəri artır. click == 5 yani əgər butona 5 dəfə kliklənibsə...
     if(clicks == 5)
     {
-        confirmAction()
+        confirmAction() //bu funksiya aşağıda qeyd olunub, boş verin, lazımsızdı
     }
-    else if(clicks == 10)
+    else if(clicks == 10) //eyni şəkildə buton 10 dəfə kliklənibsə..
     {
-        alert("Bezmədin?")
+        alert("Bezmədin?") //alerti səhv etmirəmsə dərsdə görmüşük
     }
     else if(clicks == 15)
     {
@@ -42,7 +52,7 @@ function startGame(e) {
     }
     else if(clicks == 17)
     {
-        mike.style.display = 'block'
+        mike.style.display = 'block' //17 klikdə artıq yaratdığımız classa inline olaraq display block əıavə edib şəkili visible edirik ;)
     }
     
 }
